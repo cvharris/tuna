4db7f03cc0ab5710f2d8c66896d6799d32d74f04
Charlie Harris <charris1014@gmail.com>
2023-06-16 23:14:23 -0500

    Adds a barcdoe scanner example that talks to OFF API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -54,18 +56,21 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Product {}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _MyHomePageState extends State<MyHomePage> {
+  String _barcode = '';
+
+  Future<void> scanBarcode() async {
+    // Get the barcode scanner plugin
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#ea9b06", "Cancel", false, ScanMode.BARCODE);
+    debugPrint('movieTitle: $barcodeScanRes');
+
+    http.Response res = await http.get(Uri.parse(
+        'https://world.openfoodfacts.org/api/v1/product/$barcodeScanRes.json'));
+
+    _barcode = barcodeScanRes;
   }
 
   @override
@@ -106,18 +111,18 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Scan a Barcode, it will appear here:',
             ),
             Text(
-              '$_counter',
+              '$_barcode',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: scanBarcode,
+        tooltip: 'Scan Barcode',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
